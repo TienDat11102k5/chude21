5859e15363e2507886f6d41cb6d1462ef2ada3d0
TienDat1102k5 <dodatsdf282@gmail.com>
2024-12-27 16:14:51 +0700

    Update settings.py

--- wed21/QL_Care_Health/QL_Care_Health/settings.py
+++ wed21/QL_Care_Health/QL_Care_Health/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'QL_Care_Health.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'DjangoDB',
+		'USER': 'root',
+		'PASSWORD': '1234',
+		'HOST':'127.0.0.1',
+		'PORT':'3306',
+	}
 }
+#MY SQL
 
 
 # Password validation
