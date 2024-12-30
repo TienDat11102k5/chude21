7ed6587a403683661c633842cc378e80e22b7993
TienDat1102k5 <dodatsdf282@gmail.com>
2024-12-30 20:44:31 +0700

    LÀM ƠN ĐỪNG SỮA BẬY NỮA

--- wed21/QL_Care_Health/Customer_MNG/models.py
+++ wed21/QL_Care_Health/Customer_MNG/models.py
@@ -2,15 +2,13 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-
+from django.db import models
 
 class Customer(models.Model):
-    customer_id = models.AutoField(primary_key=True)
-    user_id = models.IntegerField()
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=254)
-    address = models.CharField(max_length=255)
-
+    email = models.EmailField()
 
     def __str__(self):
-        return f"{self.name} - {self.address}"
+        return self.name
+
+
