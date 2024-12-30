5651d8b47d898c20ae3dbb6ee1839f20aa784598
Dat-Pham-05 <datpham100705@gmail.com>
2024-12-30 20:12:11 +0700

    oke

--- wed21/QL_Care_Health/Customer_MNG/models.py
+++ wed21/QL_Care_Health/Customer_MNG/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+
 class Customer(models.Model):
     customer_id = models.AutoField(primary_key=True)
     user_id = models.IntegerField()
