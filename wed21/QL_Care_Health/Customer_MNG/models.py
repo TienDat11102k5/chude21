20da13fc5f3f0d8c12998232e3fd008fe907bb75
Dat-Pham-05 <datpham100705@gmail.com>
2024-12-30 17:54:09 +0700

    thay thế model customer

--- wed21/QL_Care_Health/Customer_MNG/models.py
+++ wed21/QL_Care_Health/Customer_MNG/models.py
@@ -4,8 +4,11 @@ from django.db import models
 from django.db import models
 
 class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField()
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    address = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.address}"
