dcd54d9637c55932a9d22dc505af8ff731000a16
Duy-1403 <duyc914032005@gmail.com>
2024-12-30 18:10:50 +0700

    class
    1

--- wed21/QL_Care_Health/Employee_MNG/models.py
+++ wed21/QL_Care_Health/Employee_MNG/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+   employeeID = models.CharField(max_lenghth=50)
+   usesID = models.CharField(max_length=50)
+def _str_ (self):
+    return self.employeeID
--- wed21/QL_Care_Health/Guest_MNG/models.py
+++ wed21/QL_Care_Health/Guest_MNG/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Guest(models.Model):
+  name = models.CharField(max_length=100)
+  usesID = models.CharField(max_length=50)
+  birthday = models.CharFiedl(max_length=50)
+  phone = models.CharField(max_length=50)
+  gender = models.CharField(boolean)
+  email = models.EmailField(max_length=100)
+  roleID = models.CharField(max_length=100)
+def _str_ (self):
+ return self.name
