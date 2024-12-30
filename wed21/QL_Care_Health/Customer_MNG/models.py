09ed2169b6bd9bb71985acd53f6319c916a623c9
Duy-1403 <duyc914032005@gmail.com>
2024-12-30 18:10:50 +0700

    class
    1

--- wed21/QL_Care_Health/Customer_MNG/models.py
+++ wed21/QL_Care_Health/Customer_MNG/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.db import models
 
 class Customer(models.Model):
+<<<<<<< Updated upstream
     customer_id = models.AutoField(primary_key=True)
     user_id = models.IntegerField()
     name = models.CharField(max_length=100)
@@ -12,3 +13,10 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.address}"
+=======
+    customerID = models.CharField(max_length=50)
+    usesID = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    def _str_ (self):
+        return self.customerID
+>>>>>>> Stashed changes
