a6ef92103f01bce426284453039e84a1bd978078
Dat-Pham-05 <datpham100705@gmail.com>
2025-01-02 11:04:59 +0700

    sss

--- wed21/QL_Care_Health/QL_Care_Health/wsgi.py
+++ wed21/QL_Care_Health/QL_Care_Health/wsgi.py
@@ -7,6 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+
+
+
+#123124133155353
 import os
 
 from django.core.wsgi import get_wsgi_application
