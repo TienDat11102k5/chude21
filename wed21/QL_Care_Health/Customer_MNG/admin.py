b8be4efa4540af2c2b4f711418c52d6d98d36cdc
TienDat1102k5 <dodatsdf282@gmail.com>
2025-01-03 14:58:23 +0700

    footer

--- wed21/QL_Care_Health/Customer_MNG/admin.py
+++ wed21/QL_Care_Health/Customer_MNG/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Customer
-
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('customer_id', 'user_id', 'address')
     search_fields = ('address',)
+
