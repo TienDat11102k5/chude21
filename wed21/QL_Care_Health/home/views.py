fcedd8dd503c58dfc3296548eda7789cb68483b6
TienDat1102k5 <dodatsdf282@gmail.com>
2025-01-03 15:51:11 +0700

    master template 5

--- wed21/QL_Care_Health/home/views.py
+++ wed21/QL_Care_Health/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'home/hom9.html')
