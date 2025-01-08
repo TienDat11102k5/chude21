26e1d9127428ce5be7d0840780521a59a922745f
TienDat1102k5 <dodatsdf282@gmail.com>
2025-01-08 10:39:31 +0700

    menu update

--- wed21/QL_Care_Health/QL_Care_Health/urls.py
+++ wed21/QL_Care_Health/QL_Care_Health/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
 urlpatterns = [
     path('Customer_MNG/', include('Customer_MNG.urls')),
     path('', include ('home.urls')),
