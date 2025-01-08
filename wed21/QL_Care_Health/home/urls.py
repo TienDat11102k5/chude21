11957a7a0fc299ccb0f81f28c948adf629c7ffdb
Duy-1403 <duyc914032005@gmail.com>
2025-01-08 15:20:14 +0700

    update

--- wed21/QL_Care_Health/home/urls.py
+++ wed21/QL_Care_Health/home/urls.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from django.urls import include, path
 from .import views
 urlpatterns = [
-    path('', views.home, name ="home")
+    path('', views.home, name ="home"),
+    path('index.html', views.index, name='index'),
+    path('quan-ly-thu-cung.html', views.pet_management, name='pet_management'),
+    path('lich-su-kham.html', views.examination_history, name='history_exam'),
+    path('dang-ky-kham.html', views.dang_ky_kham_view, name='register_exam'),
+    path('checkin-thu-cung.html', views.checkin_thu_cung_view, name='checkin_pet'),
+    path('danh-gia-kham.html', views.danh_gia_kham_view, name='evaluate_exam'),
+    path('theo-doi-nhap-vien.html', views.theo_doi_nhap_vien_view, name='track_admission'),
+    path('quan-ly-booking.html', views.quan_ly_booking_view, name='manage_booking'),
+    path('quan-ly-chuong.html', views.quan_ly_chuong_view, name='manage_kennel'),
+    path('cap-nhat-thong-tin-chuong.html', views.cap_nhat_thong_tin_chuong_view, name='update_kennel'),
+    path('sap-lich-kham.html', views.sap_lich_kham_view, name='schedule_exam'),
+    path('sap-lich-nhap-vien.html', views.sap_lich_nhap_vien_view, name='schedule_admission'),
+    path('thoi-gian-kham.html', views.thoi_gian_kham_view, name='exam_time'),
+    path('ghi-nhan-kham.html', views.ghi_nhan_kham_view, name='record_exam'),
+    path('cham-soc-nhap-vien.html', views.cham_soc_nhap_vien_view, name='care_admission'),
+    path('tao-tai-khoan-khach-hang.html', views.create_customer_account, name='create_customer_account'),
+    path('tao-tai-khoan-nhan-vien.html', views.create_employee_account, name='create_employee_account'),
+    path('tao-tai-khoan-bac-si.html', views.create_doctor_account, name='create_doctor_account'),
+    path('cau-hinh-he-thong.html', views.system_configuration, name='system_configuration'),
+    path('thong-ke-hoat-dong.html', views.activity_statistics, name='activity_statistics'),
 ]
--- wed21/QL_Care_Health/home/views.py
+++ wed21/QL_Care_Health/home/views.py
@@ -4,3 +4,43 @@ from django.template import loader
 # Create your views here.
 def home(request):
     return render(request, 'home/hom9.html')
+def index(request):
+    return render(request, 'index.html')
+def pet_management(request):
+    return render(request, 'quan-ly-thu-cung.html')
+def examination_history(request):
+    return render(request, 'lich-su-kham.html')
+def dang_ky_kham_view(request):
+    return render(request, 'dang-ky-kham.html')
+def checkin_thu_cung_view(request):
+    return render(request, 'checkin-thu-cung.html')
+def danh_gia_kham_view(request):
+    return render(request, 'danh-gia-kham.html')
+def theo_doi_nhap_vien_view(request):
+    return render(request, 'theo-doi-nhap-vien.html')
+def quan_ly_booking_view(request):
+    return render(request, 'quan-ly-booking.html')
+def quan_ly_chuong_view(request):
+    return render(request, 'quan-ly-chuong.html')
+def cap_nhat_thong_tin_chuong_view(request):
+    return render(request, 'cap-nhat-thong-tin-chuong.html')
+def sap_lich_kham_view(request):
+    return render(request, 'sap-lich-kham.html')
+def sap_lich_nhap_vien_view(request):
+    return render(request, 'sap-lich-nhap-vien.html')
+def thoi_gian_kham_view(request):
+    return render(request, 'thoi-gian-kham.html')
+def ghi_nhan_kham_view(request):
+    return render(request, 'ghi-nhan-kham.html')
+def cham_soc_nhap_vien_view(request):
+    return render(request, 'cham-soc-nhap-vien.html')
+def create_customer_account(request):
+    return render(request, 'tao-tai-khoan-khach-hang.html')
+def create_employee_account(request):
+    return render(request, 'tao-tai-khoan-nhan-vien.html')
+def create_doctor_account(request):
+    return render(request, 'tao-tai-khoan-bac-si.html')
+def system_configuration(request):
+    return render(request, 'cau-hinh-he-thong.html')
+def activity_statistics(request):
+    return render(request, 'thong-ke-hoat-dong.html')
