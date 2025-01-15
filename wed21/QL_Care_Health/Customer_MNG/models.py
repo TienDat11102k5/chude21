aa6de4346ab83a63fd11c0526866637f783fb851
TienDat1102k5 <dodatsdf282@gmail.com>
2025-01-15 14:29:49 +0700

    thêm_pass vào customer

--- wed21/QL_Care_Health/Customer_MNG/models.py
+++ wed21/QL_Care_Health/Customer_MNG/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+from django.contrib.auth.hashers import make_password, check_password
+
 class Customer(models.Model):
     customer_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
@@ -9,6 +11,13 @@ class Customer(models.Model):
     address = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     birthday = models.DateField(blank=True, null=True)
+    password = models.CharField(max_length=128) 
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
 
     def __str__(self):
         return self.name
